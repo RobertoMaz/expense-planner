69c6396497ad3a804968d95bd0c826d9cab96aa2
Roberto Mazzarello <rjmazzarello@gmail.com>
2024-04-24 23:29:28 -0300

    pasamos el budgestate como prop al budgetmanagment

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,7 @@
         />
         <BudgetManagement 
           v-else
+          :budgetState="budgetState"
         />
 
       </div>
--- src/components/BudgetManagement.vue
+++ src/components/BudgetManagement.vue
@@ -1,5 +1,12 @@
 <script setup>
-   import image from '../assets/img/graphic.jpg'
+    import image from '../assets/img/graphic.jpg'
+
+    const props = defineProps({
+        budgetState: {
+            type: Number,
+            required: true
+        }
+    })
 </script>
 
 <template>
@@ -10,12 +17,79 @@
             />
         </div>
         <div class="container-budget">
-
+            <button class="reset-app">
+                Resetear App
+            </button>
+            <p>
+                <span>Presupuesto:</span> ${{ budgetState }}
+            </p>
+            <p>
+                <span>Disponible:</span> ${{ budgetState }}
+            </p>
+            <p>
+                <span>Gastado:</span> $0
+            </p>
         </div>
     </div>
 </template>
 
 
 <style scoped>
+    .container-budget {
+        width: 100%;
+    }
 
+    .container-budget p {
+        font-size: 2.4rem;
+        text-align: center;
+        color: var(--gray-dark);
+    }
+    
+    .container-budget span {
+        color: var(--blue);
+        font-weight: 900;
+    }
+    
+    .container-graphic {
+        margin-bottom: 4rem;
+    }
+
+    .reset-app {
+        background-color: #dd3838;
+        border: none;
+        padding: 1rem;
+        width: 100%;
+        color: var(--white);
+        font-weight: 900;
+        text-transform: uppercase;
+        border-radius: 1rem;
+        transition-property: background-color;
+        transition-duration: 300ms;
+    }
+
+    .reset-app:hover {
+        cursor: pointer;
+        background-color: #bd2727;
+    }
+    
+    .two-columns {
+        display: flex;
+        flex-direction: column;
+    }
+    
+    @media (min-width: 768px) {
+        .container-budget p {
+            text-align: left;
+        }
+
+        .container-graphic {
+            margin-bottom: 0rem;
+        }
+        
+        .two-columns {
+            flex-direction: row;
+            gap: 4rem;
+            align-items: center;
+        }
+    }
 </style>
